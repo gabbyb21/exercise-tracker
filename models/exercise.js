44d43f978d0c6d76daf0b2846a04ce37f0b38c14
gabbyb21 <gabbailo94@gmail.com>
2023-06-23 12:56:38 -0400

    initial commit

---
+++ models/exercise.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const exerciseSchema = new Schema({
+  name: {type: String, required: true},
+  sets: Number, 
+  reps: Number, 
+  weight: String
+});
+
+module.exports = mongoose.model('Exercise', exerciseSchema);
